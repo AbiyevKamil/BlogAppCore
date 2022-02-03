8bc9b7cdc1a3056ed2aa9d9c544a06623c7fa892
Kamil Abiyev <kamilabiyev1903@gmail.com>
2022-02-03 18:54:31 +0400

    Add project files.

---
+++ BlogAppCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BlogAppCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
